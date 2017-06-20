0b8ffb2770886e2e12e053eddc332b4aa45fab38
Joshua Astray <JoshuaSRKF@gmail.com>
2017-06-20 22:42:04 +0800

    update chips

---
+++ chips/1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="1" tilewidth="48" tileheight="48" tilecount="256" columns="16">
+ <image source="1.png" width="768" height="768"/>
+</tileset>
---
+++ chips/5.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="5" tilewidth="48" tileheight="48" tilecount="8" columns="8">
+ <image source="5.png" width="384" height="48"/>
+</tileset>
